6d1a46aaf8b84c7692220a66322426c1a3eafdf8
Harsh3815 <harshkatrodiya336@gmail.com>
2025-07-14 08:22:56 +0530

    Add files via upload

---
+++ j2ee601/JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication9;
+import java.sql.*;
+/**
+ *
+ * @author KSC86
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try
+        {
+            class.forNmae("com.mysql.cj.jdbc.Driver");
+             connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","","").
+            
+        }
+    }
+    
+}
